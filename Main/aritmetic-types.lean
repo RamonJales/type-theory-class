626e025da7abc8a296105650b39c9f2e5a02234b
RamonJales <ramonjales123@gmail.com>
2025-04-16 15:30:01 -0300

    Using Sum namespace

--- Main/aritmetic-types.lean
+++ Main/aritmetic-types.lean
@@ -1,11 +1,13 @@
+open Sum
+
 -- γ ⨯ (α + β) ≅ (γ ⨯ α) + (γ ⨯ β)
 def f_distributive : (γ × (α ⊕ β)) → (γ × α) ⊕ (γ × β)
-| (w, Sum.inl a) => Sum.inl (w, a)
-| (w, Sum.inr b) => Sum.inr (w, b)
+| (w, inl a) => inl (w, a)
+| (w, inr b) => inr (w, b)
 
 def f'_distributive : (γ × α) ⊕ (γ × β) → (γ × (α ⊕ β))
-| Sum.inl (w, a) => (w, Sum.inl a)
-| Sum.inr (w, b) => (w, Sum.inr b)
+| inl (w, a) => (w, inl a)
+| inr (w, b) => (w, inr b)
 
 theorem types_distributive {γ α β : Type} :
   (∀ (x : γ × (α ⊕ β)), (f'_distributive ∘ f_distributive) x = x) ∧
@@ -19,16 +21,16 @@ by
     cases s
     rename_i a
     calc
-        (f'_distributive ∘ f_distributive) (w, Sum.inl a)
-          = f'_distributive (f_distributive ((w, Sum.inl a))) := by rfl
-        _ = f'_distributive (Sum.inl (w, a))                  := by rfl
-        _ = (w, Sum.inl a)                                    := by rfl
+        (f'_distributive ∘ f_distributive) (w, inl a)
+          = f'_distributive (f_distributive ((w, inl a))) := by rfl
+        _ = f'_distributive (inl (w, a))                  := by rfl
+        _ = (w, inl a)                                    := by rfl
     rename_i b
     calc
-        (f'_distributive ∘ f_distributive) (w, Sum.inr b)
-          = f'_distributive (f_distributive (w, Sum.inr b)) := by rfl
-        _ = f'_distributive (Sum.inr (w, b))                := by rfl
-        _ = (w, Sum.inr b)                                  := by rfl
+        (f'_distributive ∘ f_distributive) (w, inr b)
+          = f'_distributive (f_distributive (w, inr b)) := by rfl
+        _ = f'_distributive (inr (w, b))                := by rfl
+        _ = (w, inr b)                                  := by rfl
     -- And right case:
     intro y
     cases y
@@ -36,15 +38,15 @@ by
     cases p
     rename_i w a
     calc
-        (f_distributive ∘ f'_distributive) (Sum.inl (w, a))
-            = f_distributive (f'_distributive ((Sum.inl (w, a)))) := by rfl
-          _ = f_distributive (w, Sum.inl a)                       := by rfl
-          _ = Sum.inl (w, a)                                      := by rfl
+        (f_distributive ∘ f'_distributive) (inl (w, a))
+            = f_distributive (f'_distributive ((inl (w, a)))) := by rfl
+          _ = f_distributive (w, inl a)                       := by rfl
+          _ = inl (w, a)                                      := by rfl
     rename_i p
     cases p
     rename_i w b
     calc
-        (f_distributive ∘ f'_distributive) (Sum.inr (w, b))
-            = f_distributive (f'_distributive ((Sum.inr (w, b))))   := by rfl
-          _ = f_distributive (w, Sum.inr b)                         := by rfl
-          _ = Sum.inr (w, b)                                        := by rfl
+        (f_distributive ∘ f'_distributive) (inr (w, b))
+            = f_distributive (f'_distributive ((inr (w, b))))   := by rfl
+          _ = f_distributive (w, inr b)                         := by rfl
+          _ = inr (w, b)                                        := by rfl
